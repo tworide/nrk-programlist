704fb8e776b7ef507ad7aa324b3dde65fcc91f98
Tor Ivar Johansen <toij@ifi.uio.no>
2012-09-11 02:22:24 +0200

    * Added class for building url with params

---
+++ urlutils.py
@@ -0,0 +1,12 @@
+class UrlBuilder:
+    def __init__(self, domain, path, params):
+        self.domain = domain
+        self.path = path
+        self.params = params
+
+    def __str__(self):
+        return ""
+
+
+if __name__ == '__main__':
+    print "hello world"
