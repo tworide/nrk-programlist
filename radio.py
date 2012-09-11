fefd259bb90f09ce8b1f447eef03aebb042f86b6
Tor Ivar Johansen <toij@ifi.uio.no>
2012-09-11 04:44:52 +0200

    * Print program list using new UrlBuilder

--- radio.py
+++ radio.py
@@ -4,9 +4,16 @@ import re
 import time
 import urllib2
 from bs4 import BeautifulSoup
+import urlutils
 
 #response = urllib2.urlopen('http://python.org/')
-response = urllib2.urlopen('http://www.nrk.no/programoversikt/avansert/?p_artikkel_id=&p_forhandsvis_flg=0&p_format=HTML&p_type=prog&p_fom_dag=11&p_fom_mnd=9&p_fom_ar=2012&p_periode=dennedagen&p_p1=P1&p_soketekst=&p_knapp=Vis+nedenfor')
+
+urlbuild = urlutils.UrlBuilder(query='p_format=HTML')
+plist = urlutils.ProgramListParams()
+plist.setChannel("P1")
+urlbuild.setQuery(plist)
+
+response = urllib2.urlopen(urlbuild.__str__())
 html = response.read()
 
 soup = BeautifulSoup(html)
@@ -18,7 +25,5 @@ programs = soup.find_all("div", "pubprogtittel")
 
 now = time.strftime("%H:%M")
 
-print now
-
-# for starttime, program in zip(starttimes,programs):
-#     print starttime.get_text(), 'starts: ', program.get_text()
+for starttime, program in zip(starttimes,programs):
+    print starttime.get_text(), 'starts: ', program.get_text()
